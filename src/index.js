4ce21f6da8db9e83d7463d5bf013d2aa8549f8d1
Jakub Svehla <svehl.jakub@gmail.com>
2018-02-03 13:43:06 +0100

    is In array

--- src/index.js
+++ src/index.js
@@ -16,16 +16,11 @@ const binaryPipeComp =
   binaryPipeComp1 >> R.map(R.multiply(2))
 
 
+const isInArr = 5 >> R.contains << [1,2,3,4]
 
 console.log(numbers)
 console.log(someOperatoins)
 console.log(binaryPipeComp1)
 console.log(binaryPipeComp)
 console.log(is5LargerThan4)
-
-const add3 = x => x + 3
-const add2 = x => x + 2
-
-const y = 5 >> R.contains << [1,2,3,4]
-
-console.log(y)
+console.log(isInArr)
