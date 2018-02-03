6142f419372c3c88bdb685cdfd093d5d78c5d4cc
Jakub Svehla <svehl.jakub@gmail.com>
2018-02-03 01:58:57 +0100

    change example for pipeline operator

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 const R = require('ramda')
 const o = { a: { } };
 const lol = o?.a?.b
-const numbers = [1,2,3,10] |> R.filter(R.gt(3))
+const numbers = [1,2,3,10]
+  |> R.map(x => x * 2)
+  |> R.reduce( (a,b) => a + b )
+  |> R.add(1);
+  
 console.log(numbers); // [ 1, 2 ]
 console.log(lol); // undefined
